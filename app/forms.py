d2eaed21db24a670d32332c9a4c0522deaafed3a
Alimov Rustam <Alimov_Rustam@timelyskills.com>
2026-05-18 03:26:11 +0600

    Revert "Add Task Manager project"
    This reverts commit d73c1db90051e5aa35af264363f535e33f98cfc7.

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class TaskForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    description = TextAreaField("Description")
-    completed = BooleanField("Completed")
-    submit = SubmitField("Add Task")
